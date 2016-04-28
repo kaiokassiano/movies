747912fd280f52055cf9b95aa548e9fe7d82ab6b
Kaio <kaio.kassiano.oliveira@ccc.ufcg.edu.br>
2016-04-28 01:37:22 -0300

    Alterado o icone do aplicativo

--- app/src/main/java/com/study_jams/kaio/movies/MainActivity.java
+++ app/src/main/java/com/study_jams/kaio/movies/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -19,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
         return true;
     }
 
